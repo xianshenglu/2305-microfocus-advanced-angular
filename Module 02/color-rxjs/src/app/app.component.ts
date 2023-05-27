f424b7f9a9e987584faa89b003c016caf8dfa3c1
xianshenglu <xianshenglu@qq.com>
2023-05-28 00:38:31 +0800

    Add demo for signal

--- Module 02/color-rxjs/src/app/app.component.ts
+++ Module 02/color-rxjs/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Observable, Subject, concatAll, distinctUntilChanged, filter, interval, map, merge, mergeAll, of, switchAll, take } from 'rxjs';
+import { Component, effect } from '@angular/core';
+import { Observable, Subject, distinctUntilChanged, map, merge, switchAll, tap } from 'rxjs';
 import { Color } from './models/color.model';
 import { ColorsService } from './services/colors.service';
-
+import { signal } from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,10 +14,16 @@ export class AppComponent {
   search$ = new Subject<string>();
   results$!: Observable<Color[]>;
   isBusy$!: Observable<boolean>;
-  
+
+  search = signal("")
+  results = signal<Color[]>([])
+  isBusy = signal(false);
+
+  isSignalMode = true;
+
   constructor(private colorsService: ColorsService) {
     const res$ = this.search$.pipe(
-      map(keyword => this.colorsService.search(keyword)), 
+      map(keyword => this.colorsService.search(keyword)),
       switchAll()
       )
 
@@ -34,17 +41,40 @@ export class AppComponent {
     this.isBusy$ = merge(true$, false$).pipe(
       distinctUntilChanged()
     );
+    this.watchSearchSignal();
+    // this.watchSearchSignalObservable();
 
-     
   }
 
-
-
+  private watchSearchSignal() {
+    effect(async () => {
+      this.isBusy.set(true);
+      const keyword = this.search();
+      const results = await this.colorsService.search(keyword);
+      const isSearchedKeywordSameAsCurrent = keyword === this.search();
+      if (isSearchedKeywordSameAsCurrent) { // just a replacement for switchAll operator
+        this.isBusy.set(false);
+        this.results.set(results);
+      }
+    }, { allowSignalWrites: true });
+  }
+  private watchSearchSignalObservable() {
+    toObservable(this.search).pipe(
+      tap(() => this.isBusy.set(true)),
+      map(keyword => this.colorsService.search(keyword)),
+      switchAll(),
+      tap((results) => {
+        this.isBusy.set(false);
+        this.results.set(results);
+      }),
+      takeUntilDestroyed()
+    ).subscribe();
+  }
   // log<T>(observable: Observable<T>, prefix: string) {
   //   observable.subscribe({
-  //     next: val => console.log(`${prefix} next ${val}`), 
-  //     complete: () => console.log(`${prefix} complete`), 
-  //     error: err => console.log(`${prefix} error ${err}`), 
+  //     next: val => console.log(`${prefix} next ${val}`),
+  //     complete: () => console.log(`${prefix} complete`),
+  //     error: err => console.log(`${prefix} error ${err}`),
   //   })
   // }
 
